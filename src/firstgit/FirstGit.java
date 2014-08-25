131d5a94f824ae268c6f6f29b1750aac97be8e78
wsuetholz <wsuetholz@L11107042788.wctc.msft>
2014-08-25 18:59:32 -0500

    Say Hello World.

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package firstgit;
 
@@ -17,6 +13,7 @@ public class FirstGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println ("Hello World");
     }
     
 }
